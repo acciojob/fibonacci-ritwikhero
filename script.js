00f9e460d215a60b559cf1bef506ee8c71191723
Ritwik Singh <76203605+ritwikhero@users.noreply.github.com>
2024-03-26 13:54:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function fibonacci(num) {
 // your code here
-	let num1 = 0;
-    let num2 = 1;
-    let sum;
-    let i = 0;
-    for (i = 0; i < num; i++) {
-        sum = num1 + num2;
-        num1 = num2;
-        num2 = sum;
-    }
-    return num2;
+  if (num === 1) return 0;
+  else if (num === 2) return 1;
+  num -= 2;
+  var num1 = 0;
+  var num2 = 1;
+  var sum;
+  var i = 0;
+  for (i = 0; i < num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num2;
 }
+
+module.exports = fibonacci;
 
